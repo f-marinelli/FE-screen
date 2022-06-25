2fefb6b131b35ce92308f88e6307809197a58cf7
f-marinelli <fabri.marinelli@gmail.com>
2022-06-25 10:30:22 +0200

    stripe

--- src/components/Body/BtnKey.tsx
+++ src/components/Body/BtnKey.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Button } from 'react-bootstrap';
 import { AuthContext } from '../../context/AuthContext';
 import useSignIn from '../../hooks/useSignIn';
@@ -17,15 +17,45 @@ const BtnKey: React.FC = () => {
     handleSwitchForm,
   } = useSignIn();
 
+  useEffect(() => {
+    // Check to see if this is a redirect back from Checkout
+    const query = new URLSearchParams(window.location.search);
+
+    if (query.get('success')) {
+      console.log('Order placed! You will receive an email confirmation.');
+    }
+
+    if (query.get('canceled')) {
+      console.log(
+        "Order canceled -- continue to shop around and checkout when you're ready."
+      );
+    }
+  }, []);
+
+  // const fetchStripe = async () => {
+  //   const res = await fetch('http://localhost:4242/create-checkout-session', {
+  //     method: 'POST',
+  //   });
+
+  //   console.log(res.url);
+  // };
+
   const handleClick = () => {
-    Object.keys(user).length === 0 ? handleShowIn() : console.log('stripe');
+    Object.keys(user).length === 0 && handleShowIn();
   };
+
+  const btnType = Object.keys(user).length >= 1 ? 'submit' : 'button';
 
   return (
     <div className="container d-flex justify-content-center mt-5">
-      <Button variant="primary" onClick={handleClick}>
-        Buy A Key
-      </Button>
+      <form
+        action="http://localhost:4242/create-checkout-session"
+        method="POST"
+      >
+        <Button type={btnType} variant="primary" onClick={handleClick}>
+          Buy A Key
+        </Button>
+      </form>
       <ModalSignIn
         show={showIn}
         handleClose={handleCloseIn}
